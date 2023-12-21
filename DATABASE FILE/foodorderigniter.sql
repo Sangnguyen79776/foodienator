b4535cb2df1ad73555f3b20da81220583c27aa64
Sangnguyen79776 <145439978+Sangnguyen79776@users.noreply.github.com>
2023-12-21 10:31:11 -0600

    Update foodorderigniter.sql

--- DATABASE FILE/foodorderigniter.sql
+++ DATABASE FILE/foodorderigniter.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 17, 2021 at 04:42 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
